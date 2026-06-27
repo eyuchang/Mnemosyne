1689995a41d9e6b023e78848c3af4a6da1f65eb3
Edward Chang <eyuchang@gmail.com>
2026-06-26 18:25:32 -0700

    Add CTL serialization for active commitments

--- mnemosyne/core/commitments/__init__.py
+++ mnemosyne/core/commitments/__init__.py
@@ -8,6 +8,13 @@ from mnemosyne.core.commitments.projection import (
     CommitmentProjection,
     replay_commitments,
 )
+from mnemosyne.core.commitments.ctl import (
+    ACTIVE_COMMITMENT_EXTENSION_KIND,
+    event_to_extension,
+    event_from_extension,
+    is_commitment_extension,
+    extract_commitment_events_from_ctl_records,
+)
 
 __all__ = [
     "ActiveCommitment",
@@ -16,4 +23,9 @@ __all__ = [
     "CommitmentStatus",
     "CommitmentProjection",
     "replay_commitments",
+    "ACTIVE_COMMITMENT_EXTENSION_KIND",
+    "event_to_extension",
+    "event_from_extension",
+    "is_commitment_extension",
+    "extract_commitment_events_from_ctl_records",
 ]
